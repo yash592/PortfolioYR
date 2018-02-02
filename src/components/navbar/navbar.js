d0a6a78b77db7f688ae5861c836423d644e0f828
Yash Rajgor <yash592@gmail.com>
2018-02-01 23:57:02 -0800

    navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,1 +1,22 @@
-// 
+// nav
+
+import React from "react";
+
+const Navbar = () => (
+        <nav class="navbar navbar-light navbar-expand-md navigation-clean">
+            <div class="container"><a class="navbar-brand" href="#">Yash Rajgor</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"
+                    id="navcol-1">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#">About </a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#">Education </a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#">Skills </a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#">Portfolio </a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#">Contact </a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+);
+
+export default Navbar;
