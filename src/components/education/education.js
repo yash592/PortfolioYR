d998effeb4dcffa0c59896bb2fa20b838f83deef
Yash Rajgor <yash592@gmail.com>
2018-02-04 23:27:54 -0800

    More timeline

--- src/components/education/education.js
+++ src/components/education/education.js
@@ -6,66 +6,72 @@ import "./education.css";
 const education = () => (
   <div class="container">
                 <header class="clearfix">
-                    <span>Blueprint <span class="bp-icon bp-icon-about" data-content="The Blueprints are a collection of basic and minimal website concepts, components, plugins and layouts with minimal style for easy adaption and usage, or simply for inspiration."></span></span>
-                    <h1>Vertical Timeline</h1>
-                    
+                    <h1>Education and Work Experience</h1>
+
                 </header>
                 <div class="main">
                     <ul class="cbp_tmtimeline">
                         <li>
-                            <time class="cbp_tmtime" datetime="2013-04-10 18:30"><span>4/10/13</span> <span>18:30</span></time>
+                            <time class="cbp_tmtime" datetime="2013-04-10 18:30"> <span>05/13</span></time>
                             <div class="cbp_tmicon cbp_tmicon-phone"></div>
                             <div class="cbp_tmlabel">
-                                <h2>Ricebean black-eyed pea</h2>
-                                <p>Winter purslane courgette pumpkin quandong komatsuna fennel green bean cucumber watercress. Pea sprouts wattle seed rutabaga okra yarrow cress avocado grape radish bush tomato ricebean black-eyed pea maize eggplant. Cabbage lentil cucumber chickpea sorrel gram garbanzo plantain lotus root bok choy squash cress potato summer purslane salsify fennel horseradish dulse. Winter purslane garbanzo artichoke broccoli lentil corn okra silver beet celery quandong. Plantain salad beetroot bunya nuts black-eyed pea collard greens radish water spinach gourd chicory prairie turnip avocado sierra leone bologi.</p>
+                                <h2>Bachelors in Aerospace and Mechanical Engineering</h2>
+                                <h3>Illinois Institute of Technology</h3>
                             </div>
                         </li>
                         <li>
-                            <time class="cbp_tmtime" datetime="2013-04-11T12:04"><span>4/11/13</span> <span>12:04</span></time>
+                            <time class="cbp_tmtime" datetime="2013-04-11T12:04"><span>10/13</span></time>
                             <div class="cbp_tmicon cbp_tmicon-screen"></div>
                             <div class="cbp_tmlabel">
-                                <h2>Greens radish arugula</h2>
-                                <p>Caulie dandelion maize lentil collard greens radish arugula sweet pepper water spinach kombu courgette lettuce. Celery coriander bitterleaf epazote radicchio shallot winter purslane collard greens spring onion squash lentil. Artichoke salad bamboo shoot black-eyed pea brussels sprout garlic kohlrabi.</p>
+                                <h2>Jr. Mechanical Engineer Intern</h2>
+                                <h3>Cetron Gears</h3>
+                                <ul>
+                                  <li> Created assembly drawings, including bill of materials and detailed drawings on AutoCAD for machining, assembly and welding of gear units.</li>
+                                  <li>Worked with senior engineers on machining parts using CNC machines like Lathes and Milling Machines.</li>
+                                  <li>Acquired an understanding of CNC programming for machining parts of the gear units.</li>
+                                </ul>
                             </div>
                         </li>
                         <li>
-                            <time class="cbp_tmtime" datetime="2013-04-13 05:36"><span>4/13/13</span> <span>05:36</span></time>
+                            <time class="cbp_tmtime" datetime="2013-04-13 05:36"><span>05/14</span></time>
                             <div class="cbp_tmicon cbp_tmicon-mail"></div>
                             <div class="cbp_tmlabel">
-                                <h2>Sprout garlic kohlrabi</h2>
-                                <p>Parsnip lotus root celery yarrow seakale tomato collard greens tigernut epazote ricebean melon tomatillo soybean chicory broccoli beet greens peanut salad. Lotus root burdock bell pepper chickweed shallot groundnut pea sprouts welsh onion wattle seed pea salsify turnip scallion peanut arugula bamboo shoot onion swiss chard. Avocado tomato peanut soko amaranth grape fennel chickweed mung bean soybean endive squash beet greens carrot chicory green bean. Tigernut dandelion sea lettuce garlic daikon courgette celery maize parsley komatsuna black-eyed pea bell pepper aubergine cauliflower zucchini. Quandong pea chickweed tomatillo quandong cauliflower spinach water spinach.</p>
+                                <h2>PLM Consultant (Onsite - Case New Holland America)</h2>
+                                <h3>Piterion North America</h3>
+                                <ul>
+                                  <li>Trained and assisted engineers with Teamcenter, Process workflows and IPEM processes.</li>
+                                  <li>Multisite Data Management with Teamcenter 8.3.</li>
+                                  <li>Troubleshooting issues related to Teamcenter Automotive engineering 8.3, IPEM 8.2/8.6, Vismockup, CATIA, E-Viewer and Enovia Applications.</li>
+                                  <li>Multisite remote import and export of data.</li>
+                                  <li>Responsible for managing data of multiple production servers based in Europe, Asia, South America and North America.</li>
+                                  <li>Thorough experience with Teamcenter Modules: Product Structure Manager (BOM), Organization, Workflow, Query Builder and Variants.</li>
+                                </ul>
                             </div>
                         </li>
                         <li>
-                            <time class="cbp_tmtime" datetime="2013-04-15 13:15"><span>4/15/13</span> <span>13:15</span></time>
+                            <time class="cbp_tmtime" datetime="2013-04-15 13:15"><span>07/16</span></time>
                             <div class="cbp_tmicon cbp_tmicon-phone"></div>
                             <div class="cbp_tmlabel">
-                                <h2>Watercress ricebean</h2>
-                                <p>Peanut gourd nori welsh onion rock melon mustard jícama. Desert raisin amaranth kombu aubergine kale seakale brussels sprout pea. Black-eyed pea celtuce bamboo shoot salad kohlrabi leek squash prairie turnip catsear rock melon chard taro broccoli turnip greens. Fennel quandong potato watercress ricebean swiss chard garbanzo. Endive daikon brussels sprout lotus root silver beet epazote melon shallot.</p>
+                                <h2>Teamcenter Implementation Consultant UMD Hyperloop (Onsite - University of Maryland)</h2>
+                                <h3>Piterion North America</h3>
+                                <ul>
+                                  <li>Requirements gathering for the University of Maryland Hyperloop competition team for better CAD data management.</li>
+                                  <li>Setting up Teamcenter on the server side and 4 tier rich client installation for 29 students.</li>
+                                  <li>Understanding and building the organization for the design team.</li>
+                                  <li>Setting up the NX integration with Teamcenter and importing the NX data into Teamcenter.</li>
+                                  <li>Building a simple and streamlined change management system for an efficient design process.</li>
+                                  <li>Designing workflows based on the requirements using the Workflow Designer module in Teamcenter.</li>
+                                  <li>Trained and assisted the students about Teamcenter and NX and provided them with documentation.</li>
+                                  <li>Worked on enhancing the organization/design process based on the feedback from the users as well as being a Teamcenter Administrator.</li>
+                              </ul>
                             </div>
                         </li>
                         <li>
-                            <time class="cbp_tmtime" datetime="2013-04-16 21:30"><span>4/16/13</span> <span>21:30</span></time>
+                            <time class="cbp_tmtime" datetime="2013-04-16 21:30"><span>10/17</span></time>
                             <div class="cbp_tmicon cbp_tmicon-earth"></div>
                             <div class="cbp_tmlabel">
-                                <h2>Courgette daikon</h2>
-                                <p>Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <time class="cbp_tmtime" datetime="2013-04-17 12:11"><span>4/17/13</span> <span>12:11</span></time>
-                            <div class="cbp_tmicon cbp_tmicon-screen"></div>
-                            <div class="cbp_tmlabel">
-                                <h2>Greens radish arugula</h2>
-                                <p>Caulie dandelion maize lentil collard greens radish arugula sweet pepper water spinach kombu courgette lettuce. Celery coriander bitterleaf epazote radicchio shallot winter purslane collard greens spring onion squash lentil. Artichoke salad bamboo shoot black-eyed pea brussels sprout garlic kohlrabi.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <time class="cbp_tmtime" datetime="2013-04-18 09:56"><span>4/18/13</span> <span>09:56</span></time>
-                            <div class="cbp_tmicon cbp_tmicon-phone"></div>
-                            <div class="cbp_tmlabel">
-                                <h2>Sprout garlic kohlrabi</h2>
-                                <p>Parsnip lotus root celery yarrow seakale tomato collard greens tigernut epazote ricebean melon tomatillo soybean chicory broccoli beet greens peanut salad. Lotus root burdock bell pepper chickweed shallot groundnut pea sprouts welsh onion wattle seed pea salsify turnip scallion peanut arugula bamboo shoot onion swiss chard. Avocado tomato peanut soko amaranth grape fennel chickweed mung bean soybean endive squash beet greens carrot chicory green bean. Tigernut dandelion sea lettuce garlic daikon courgette celery maize parsley komatsuna black-eyed pea bell pepper aubergine cauliflower zucchini. Quandong pea chickweed tomatillo quandong cauliflower spinach water spinach.</p>
+                                <h2>Full Stack Development</h2>
+                                <h3>UCLA Extension</h3>
                             </div>
                         </li>
                     </ul>
